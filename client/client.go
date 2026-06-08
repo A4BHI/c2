239ceec33f5b46aa11882f7eae75e7dcdd6be91e
Abhijith H Nair <manthukafc@gmail.com>
2026-06-08 14:27:40 +0530

    g

--- client/client.go
+++ client/client.go
@@ -1,52 +1,35 @@
 package main
 
 import (
-	"bufio"
 	"context"
-	"fmt"
-	"os"
 
 	"github.com/coder/websocket"
-	"github.com/coder/websocket/wsjson"
 )
 
 type c2 struct {
 	con *websocket.Conn
 }
 
-func (c c2) connectToserver() {
+func (c *c2) connectToserver() {
 	ctx := context.Background()
 	var err error
 	c.con, _, err = websocket.Dial(ctx, "ws://localhost:8080/connect", nil)
 	if err != nil {
 		return
 	}
+	go c.heartbeat()
+	go c.listenToserver()
 }
 
-func (c c2) listenToserver() {
+func (c *c2) listenToserver() {
+
+}
+
+func (c *c2) heartbeat() {
 
 }
 
 func main() {
-
-	var msg string
-	scanner := bufio.NewScanner(os.Stdin)
-
-	for {
-		fmt.Println("enter your message: ")
-		scanner.Scan()
-		msg = scanner.Text()
-		if msg == "exit" {
-			break
-		}
-		err = wsjson.Write(ctx, c, msg)
-		if err != nil {
-			fmt.Println(err)
-		}
-		var v string
-		wsjson.Read(ctx, c, v)
-		fmt.Println("recieved : ", v)
-	}
-
-	c.Close(websocket.StatusNormalClosure, "")
+	c := c2{}
+	c.connectToserver()
 }
