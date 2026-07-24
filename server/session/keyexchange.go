54ae2b8f4e046fca9590432febc32ff3923195e2
Abhijith H Nair <manthukafc@gmail.com>
2026-07-24 15:14:47 +0530

    ASDASD

--- server/session/keyexchange.go
+++ server/session/keyexchange.go
@@ -1,8 +1,19 @@
 package session
 
-import "crypto/ecdh"
+import (
+	"crypto/ecdh"
+	"crypto/rand"
+	"log"
+)
 
-func CalculateKeys() {
+func CalculateKeys() []byte {
 	curve := ecdh.X25519()
+	serverpriv, err := curve.GenerateKey(rand.Reader)
+	if err != nil {
+		log.Println("Error generating key ", err)
+		return nil
+	}
+
+	return serverpriv.Bytes()
 
 }
